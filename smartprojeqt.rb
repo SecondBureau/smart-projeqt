6718527fe73b3d1dbde9addd11c71e8cee853ab3
Gilles Crofils <gilles@secondbureau.com>
2011-11-15 16:59:53 +0800

    bugfix newrelic

--- smartprojeqt.rb
+++ smartprojeqt.rb
@@ -1,20 +1,44 @@
 require 'rubygems'
 require 'sinatra'
 #require 'datamapper'
+require 'net/http'
+require 'uri'
+
+### New Relic
+configure :production do
+  require 'newrelic_rpm'
+end
 
 get '/en' do
-  redirect 'http://projeqt.com/secondbureau#lsi0ci22621q' 
+  activate_daryl
+  redirect 'http://projeqt.com/secondbureau#lsi0ci22621q'
 end
 
 get '/fr' do
-  redirect 'http://projeqt.com/secondbureau#lsi0ci22621q' 
+  active_daryl
+  redirect 'http://projeqt.com/secondbureau#lsi0ci22621q'
 end
 
 get '/newrelic_secondbureau' do
   'ok Newrelic'
 end
 
-get %r{(.*)} do |c| 
+get %r{(.*)} do |c|
+  activate_daryl
   redirect "http://projeqt.com/secondbureau##{c}"
+end
+
+def activate_daryl
+  return unless request.env['HTTP_REFERER'].nil?
+  daryl = ENV['DARYL'] || 'daryl.2bu.ro'
+  url = "http://#{daryl}/page/create"
+  agent = request.env["HTTP_USER_AGENT"]
+  host = request.host
+  uri = request.fullpath
+  ip = request.env['HTTP_X_REAL_IP']
+  #logger.warn(request.env.inspect)
+  Thread.new do
+    Net::HTTP.post_form(URI.parse(url), {'page[ip]'=>ip, 'page[host]'=>host, 'page[agent]' => agent, 'page[uri]' => uri})
+  end
 end
 

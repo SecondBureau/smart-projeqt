ce2e1c3ce129bf3a7811beecf37a7feeda6832de
Gilles Crofils <gilles@secondbureau.com>
2011-11-15 20:10:51 +0800

    bugfix

--- smartprojeqt.rb
+++ smartprojeqt.rb
@@ -4,35 +4,6 @@ require 'sinatra'
 require 'net/http'
 require 'uri'
 
-### New Relic
-configure :production do
-  require 'newrelic_rpm'
-end
-
-get '/en' do
-  activate_daryl
-  redirect 'http://projeqt.com/secondbureau#lsi92876ci22621q'
-end
-
-get '/fr' do
-  active_daryl
-  redirect 'http://projeqt.com/secondbureau#lsi92862ci22619q'
-end
-
-get '/cn' do
-  active_daryl
-  redirect 'http://projeqt.com/secondbureau#lsi93363ci22762q'
-end
-
-get '/newrelic_secondbureau' do
-  'ok Newrelic'
-end
-
-get %r{(.*)} do |c|
-  activate_daryl
-  redirect "http://projeqt.com/secondbureau##{c}"
-end
-
 def activate_daryl
   return unless request.env['HTTP_REFERER'].nil?
   daryl = ENV['DARYL'] || 'daryl.2bu.ro'
@@ -46,4 +17,35 @@ def activate_daryl
     Net::HTTP.post_form(URI.parse(url), {'page[ip]'=>ip, 'page[host]'=>host, 'page[agent]' => agent, 'page[uri]' => uri})
   end
 end
+
+### New Relic
+configure :production do
+  require 'newrelic_rpm'
+end
+
+get '/en' do
+  #activate_daryl
+  redirect 'http://projeqt.com/secondbureau#lsi92876ci22621q'
+end
+
+get '/fr' do
+  #active_daryl
+  redirect 'http://projeqt.com/secondbureau#lsi92862ci22619q'
+end
+
+get '/cn' do
+  #active_daryl
+  redirect 'http://projeqt.com/secondbureau#lsi93363ci22762q'
+end
+
+get '/newrelic_secondbureau' do
+  'ok Newrelic'
+end
+
+get %r{(.*)} do |c|
+  #activate_daryl
+  redirect "http://projeqt.com/secondbureau##{c}"
+end
+
+
 
